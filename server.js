543e1bd67082157974c1beb4b53b1d0dff99b9b8
Stefanie Boscarino <stefanie.boscarino@gmail.com>
2017-08-22 10:03:07 -0500

    css work

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //sending emails with endGrid
 app.post('/sendEmail', (req, res) => {
